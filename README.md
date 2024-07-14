7864638551b7245da1abdae54adbc66c8e940d17
theycallhermax <mdwalters.pm@proton.me>
2024-07-14 14:11:59 -0400

    separate css styling to it's separate feature make design closer to arc browser's add building instructions

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # Ouch Browser
 
 Focus on your browsing.
+
+## Building
+
+To build Ouch, you need GTK 4 and libadwaita 1.4 installed on your system.
+
+```sh
+cargo build
+```
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,45 @@
-mod css;
+#[cfg(feature = "css")] mod css;
 mod window;
 use gtk::{glib, prelude::*};
+// use gio::{SimpleAction, prelude::*};
 
 fn main() -> glib::ExitCode {
 	let app = adw::Application::builder()
 		.application_id("ht.sr.git.shrimple.Ouch")
 		.build();
 
-	app.connect_startup(css::init);
+	// create_action(&app, "about", &about);
+
+	#[cfg(feature = "css")] app.connect_startup(css::init);
 	app.connect_activate(window::init);
 
 	app.run()
 }
+
+/* fn about() {
+	let about = adw::AboutWindow::builder()
+		.transient_for(&window::init)
+		.application_name("Ouch Browser")
+		.application_icon("ht.sr.git.shrimple.Ouch")
+		.developer_name("Shrimple Technologies")
+		.version("0.1.0")
+		.developers([
+			"Max Walters https://max.is-probably.gay/",
+			"Ally Walters"
+		])
+		.license_type(gtk::License::Gpl30)
+		.issue_url("https://todo.sr.ht/~shrimple/ouch")
+		.release_notes("
+		<ul>
+			<li>Initial preview release!</li>
+		</ul>");
+	about.present();
+}
+
+fn create_action(app: &adw::Application, name: &str, callback: &Fn) {
+	let action = gio::SimpleAction::builder()
+		.(name, None);
+	action.connect("activate", callback);
+	app.add_action(action);
+} */
+
