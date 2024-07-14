a69d6be0d01f8e66abfd821edcddc8202bbf59e3
theycallhermax <mdwalters.pm@proton.me>
2024-07-13 22:08:31 -0400

    initial commit

---
+++ src/window.rs
@@ -0,0 +1,14 @@
+use adw::prelude::*;
+
+pub fn init(app: &adw::Application) {
+	let builder = gtk::Builder::from_string(
+		include_str!("ui/window.ui")
+	);
+	let window = builder
+        .object::<adw::ApplicationWindow>("window")
+        .expect("Couldn't get window");
+
+	window.set_application(Some(app));
+
+    window.present();
+}
