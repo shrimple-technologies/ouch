37caca246f3dfe24c42b2ba0e8b01faf4ce5c9f7
theycallhermax <mdwalters.pm@proton.me>
2024-07-30 15:37:54 -0400

    add web view

--- src/window.rs
+++ src/window.rs
@@ -1,4 +1,5 @@
 use adw::prelude::*;
+use webkit6::{prelude::*, WebView};
 
 pub fn init(app: &adw::Application) {
 	let builder = gtk::Builder::from_string(
@@ -7,6 +8,14 @@ pub fn init(app: &adw::Application) {
 	let window = builder
 		.object::<adw::ApplicationWindow>("window")
 		.expect("Couldn't get window");
+	let web_view_frame = builder
+		.object::<gtk::Frame>("frame")
+		.expect("Couldn't get web view frame");
+
+	let web_view = WebView::new();
+
+	web_view.load_uri("https://google.com/");
+	web_view_frame.set_child(Some(&web_view));
 
 	window.set_application(Some(app));
 	window.present();
