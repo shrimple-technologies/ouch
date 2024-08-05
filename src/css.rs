4beb65a4faf73779d5b05379ffa586743fde9060
theycallhermax <mdwalters.pm@proton.me>
2024-08-05 17:50:24 -0400

    implement dark mode

--- src/css.rs
+++ src/css.rs
@@ -1,6 +1,12 @@
 pub fn init(_app: &adw::Application) {
 	let provider = gtk::CssProvider::new();
-	provider.load_from_string(include_str!("css/light.css"));
+	let style_manager = adw::StyleManager::default();
+
+	if style_manager.is_dark() {
+		provider.load_from_string(include_str!("css/dark.css"));
+	} else {
+		provider.load_from_string(include_str!("css/light.css"));
+	}
 
 	gtk::style_context_add_provider_for_display(
 		&gtk::gdk::Display::default().expect("Could not connect to a display."),
